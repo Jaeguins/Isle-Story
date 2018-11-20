3d3bca38c4fa154671f9299cf2385c8edf33b15f
Jaeguins <jsu4838@naver.com>
2018-11-20 14:28:36 +0900

    TriGrid raycast testing start, readme.md test adding link diary.md

--- Assets/Scripts/Terrains/TriGrid.cs
+++ Assets/Scripts/Terrains/TriGrid.cs
@@ -32,4 +32,22 @@ public class TriGrid : MonoBehaviour {
         cell.transform.localPosition = position;
         
     }
+    void Update() {
+        if (Input.GetMouseButton(0)) {
+            HandleInput();
+        }
+    }
+
+    void HandleInput() {
+        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(inputRay, out hit)) {
+            TouchCell(hit.point);
+        }
+    }
+
+    void TouchCell(Vector3 position) {
+        position = transform.InverseTransformPoint(position);
+        Debug.Log("touched at " + position);
+    }
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 # 2018 winter project for hobby
+***
+#### Development Diary:
+[2018/11/20](/Diary/Diary.md "Optional title")
