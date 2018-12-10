59e61ce8bf3c06818ee4ddae5f441644d1c521fe
Jaeguins <jsu4838@naver.com>
2018-12-10 20:50:02 +0900

    camera fader add

---
+++ Assets/Scripts/UI/Camera/Fader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fader : MonoBehaviour {
+    public RawImage fader;
+    static Fader Instance;
+    public Color color = Color.black;
+    public float speed;
+    private void Start() {
+        Instance = this;
+    }
+    public IEnumerator FadeOutInternal() {
+        color.a = 0;
+        while (color.a < 1f) {
+            Debug.Log("FO call");
+            color.a += speed;
+            fader.color = color;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+    public IEnumerator FadeInInternal() {
+        color.a = 1;
+        while (color.a >0f) {
+            Debug.Log("FI call");
+            color.a -= speed;
+            fader.color = color;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+    public static IEnumerator FadeOut() {
+        Debug.Log("FO started");
+        yield return Instance.StartCoroutine(Instance.FadeOutInternal());
+        Debug.Log("FO end");
+    }
+    public static IEnumerator FadeIn() {
+        Debug.Log("FI started");
+        yield return Instance.StartCoroutine(Instance.FadeInInternal());
+        Debug.Log("FI end");
+    }
+}
--- Assets/Scripts/UI/Selector.cs
+++ Assets/Scripts/UI/Selector.cs
@@ -12,46 +12,58 @@ public class Selector : MonoBehaviour {
     public TriMesh terrainSelectionViewer;
     public Camera cam;
     public GameObject Panel;
+    public GameObject BuildingOptionPanel;
+    public Camera buildingCam;
+    public GameObject buildingCamAxis;
     public bool showTerrain = true;
     public bool clicked = false;
-    void Update() {
-        TriCell tCell = GetRay();
-        if (!clicked) {
-            nowCell = tCell;
-            Panel.SetActive(false);
-            nowCell = tCell;
-            if (nowCell) {
-                if (showTerrain)
-                    RecalculateTerrain();
-                if (!showTerrain) {
-                    terrainSelectionViewer.Clear();
-                    terrainSelectionViewer.Apply();
-                }
+    bool inTopView = true;
+    public float buildingCamRotSpeed=1f;
+    Vector3 buildingCamAxisRot=Vector3.zero;
+    void LateUpdate() {
+        if (inTopView) {
+            TriCell tCell = GetRay();
+            if (!clicked) {
+                nowCell = tCell;
+                Panel.SetActive(false);
+                nowCell = tCell;
+                if (nowCell) {
+                    if (showTerrain)
+                        RecalculateTerrain();
+                    if (!showTerrain) {
+                        terrainSelectionViewer.Clear();
+                        terrainSelectionViewer.Apply();
+                    }
 
-                nowBuilding = nowCell.Building;
-                if (nowBuilding) {
-                    buildingUI.enabled = true;
-                    buildingNameTag.text = nowBuilding.UIName;
+                    nowBuilding = nowCell.Building;
+                    if (nowBuilding) {
+                        buildingUI.enabled = true;
+                        buildingNameTag.text = nowBuilding.UIName;
+                    }
+                    else {
+                        buildingUI.enabled = false;
+                    }
                 }
-                else {
-                    buildingUI.enabled = false;
+                buildingUI.transform.rotation = cam.transform.rotation;
+                if (Input.GetMouseButton(0) && nowCell) {
+                    clicked = true;
                 }
             }
-            buildingUI.transform.rotation = cam.transform.rotation;
-            if (Input.GetMouseButton(0)&&nowCell) {
-                clicked = true;
+            else {
+                Panel.SetActive(true);
+                if (tCell.coordinates.DistanceTo(nowCell.coordinates) > 3) {
+                    clicked = false;
+                }
             }
+        } else {
+            buildingCamAxisRot.y += buildingCamRotSpeed;
+            buildingCamAxis.transform.localRotation = Quaternion.Euler(buildingCamAxisRot);
         }
-        else {
-            Panel.SetActive(true);
-            if (tCell.coordinates.DistanceTo(nowCell.coordinates)>3) {
-                clicked = false;
-            }
-        }
-        
     }
     TriCell GetRay() {
-        return grid.GetCell(Camera.main.ScreenPointToRay(Input.mousePosition));
+        if (cam)
+            return grid.GetCell(cam.ScreenPointToRay(Input.mousePosition));
+        else return null;
     }
     public void RecalculateTerrain() {
         terrainSelectionViewer.Clear();
@@ -78,5 +90,31 @@ public class Selector : MonoBehaviour {
             terrainSelectionViewer.AddTriangleColor(Color.blue);
         }
         terrainSelectionViewer.Apply();
+    }
+    public void BuildingOption() {
+        StartCoroutine(ToBuildingCamera());
+        buildingCamAxis.transform.localPosition = nowCell.Position;
+        buildingCamAxisRot = Vector3.zero;
+        BuildingOptionPanel.SetActive(true);
+    }
+    public void ExitBuildingOption() {
+        BuildingOptionPanel.SetActive(false);
+        StartCoroutine(ToMainCamera());
+        buildingUI.enabled = true;
+        inTopView = true;
+    }
+    public IEnumerator ToBuildingCamera() {
+        yield return StartCoroutine(Fader.FadeOut());
+        inTopView = false;
+        buildingUI.enabled = false;
+        cam.enabled=false;
+        buildingCam.enabled = true;
+        yield return StartCoroutine(Fader.FadeIn());
+    }
+    public IEnumerator ToMainCamera() {
+        yield return StartCoroutine(Fader.FadeOut());
+        buildingCam.enabled = false;
+        cam.enabled = true;
+        yield return StartCoroutine(Fader.FadeIn());
     }
 }
