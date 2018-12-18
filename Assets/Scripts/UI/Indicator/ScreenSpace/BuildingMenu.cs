24f830bc135849707d74cb58fc315709d853fab3
Jaeguins <jsu4838@naver.com>
2018-12-18 17:56:39 +0900

    minimum change in building menu

--- Assets/Scripts/UI/Indicator/ScreenSpace/BuildingMenu.cs
+++ Assets/Scripts/UI/Indicator/ScreenSpace/BuildingMenu.cs
@@ -2,11 +2,20 @@
 using System.Collections;
 
 public class BuildingMenu : MonoBehaviour {
+    public Entity selected;
+    public void Bind(Entity target) {
+        selected = target;
+        gameObject.SetActive(true);
+    }
     public void buildTent() {
         Selector selector = Selector.Instance;
         selector.commandType = CommandType.BUILD;
         selector.sizeType = SizeType.SINGLE;
         selector.ordering = true;
         selector.target = TriIsleland.Instance.innPrefabs[0];
+        Hide();
+    }
+    public void Hide() {
+        gameObject.SetActive(false);
     }
 }
