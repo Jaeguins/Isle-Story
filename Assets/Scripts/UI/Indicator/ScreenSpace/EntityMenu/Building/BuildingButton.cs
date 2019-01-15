804be0c5d226f93569f7012e3ef77e64f5b67459
Jaeguins <jsu4838@naver.com>
2019-01-15 23:52:23 +0900

    unadded script add

---
+++ Assets/Scripts/UI/Indicator/ScreenSpace/EntityMenu/Building/BuildingButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BuildingButton : MonoBehaviour {
+    public Buildable Target;
+    public Button b;
+    public Entity Prefab;
+    public BuildCondition condition=new BuildCondition();
+    public void ClickCallback() {
+        Selector.Instance.RequestLocation(Target.GetCommandReceiver(),Prefab.sizeType, new BuildCommand(Prefab));
+    }
+    public void Bind(Buildable target) {
+        Target = target;
+    }
+    public virtual bool Condition() {
+        if (!Target.HasCommandReceiver()) return false;
+        if (condition.TechLevel > Target.GetTech()) return false;
+        return true;
+    }
+    public void LateUpdate() {
+        if (Target != null) {
+            b.interactable = Condition();
+        }
+    }
+}
+[System.Serializable]
+public struct BuildCondition {
+    public int TechLevel;
+}
