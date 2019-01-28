9d963e863b7eacd7af888769af270033152f1a1e
Jaeguins <jsu4838@naver.com>
2019-01-28 16:36:50 +0900

    gamescene island panel start

---
+++ Assets/Scripts/UI/Indicator/IslandView/Draggable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+    public RectTransform tp;
+    public Vector3 originalLoc;
+    public Vector3 originalCur;
+    public void Start() {
+
+    }
+    public void OnMouseDrag() {
+        tp.position = originalLoc + (Input.mousePosition- originalCur);
+        Debug.Log("dragging " + tp.position);
+    }
+    public void OnMouseDown() {
+        originalLoc = tp.position;
+        originalCur = Input.mousePosition;
+    }
+    public void OnMouseUp() {
+        
+    }
+}
---
+++ Assets/Scripts/UI/Indicator/IslandView/IslandLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IslandLoader : MonoBehaviour {
+    public string saveName;
+    public GameObject islandPrefab;
+    public Transform panel;
+    public void OnEnable() {
+        DirectoryInfo di = new DirectoryInfo(Application.persistentDataPath + "/save/" + saveName);
+        foreach (DirectoryInfo tdi in di.GetDirectories()) {
+            using (BinaryReader rd = new BinaryReader(File.OpenRead(Path.Combine(tdi.FullName, "thumbnail.dat")))) {
+                Instantiate(islandPrefab, panel);
+
+            }
+        }
+    }
+    public void Clear() {
+
+    }
+}
