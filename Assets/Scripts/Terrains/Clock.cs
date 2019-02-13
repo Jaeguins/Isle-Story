a5e18a0ce91b7e573f478cf1648097ed1b3beeb8
Jaeguins <jsu4838@naver.com>
2019-02-13 18:04:48 +0900

    day cycle temporarly fixed

--- Assets/Scripts/Terrains/Clock.cs
+++ Assets/Scripts/Terrains/Clock.cs
@@ -28,35 +28,26 @@ public class Clock : MonoBehaviour {
     }
 
     void Update() {
-        if (Updator) {
-            nightLength = secondsInNight / 2 / (secondsInDay + secondsInNight);
-            Updator = false;
-        }
         UpdateSun();
 
         currentTimeOfDay += (Time.deltaTime / (secondsInDay + secondsInNight)) * timeMultiplier;
-
+        isDay = currentTimeOfDay <= 0.625 ? true : false;
         if (currentTimeOfDay >= 1) {
             currentTimeOfDay = 0;
         }
     }
 
     void UpdateSun() {
-        sun.transform.localRotation = Quaternion.Euler((currentTimeOfDay * 360f) - 90, 170, 0);
-        
-        if (currentTimeOfDay <= nightLength || currentTimeOfDay >= 1 - nightLength) {
-            isDay = false;
-            intensityMultiplier = 0;
+        if (currentTimeOfDay <= 0.125) {
+            sun.intensity = 8*currentTimeOfDay;
         }
-        else if (currentTimeOfDay <= nightLength + 0.02f) {
-            isDay = true;
-            intensityMultiplier = Mathf.Clamp01((currentTimeOfDay - nightLength) * (1 / 0.02f));
+        else if(currentTimeOfDay <= 0.5) {
+            sun.intensity = 1;
+        }else if(currentTimeOfDay <= 0.625) {
+            sun.intensity = 8 * (0.625f-currentTimeOfDay);
         }
-        else if (currentTimeOfDay >= 0.98f - nightLength) {
-            isDay = true;
-            intensityMultiplier = Mathf.Clamp01(1 - ((currentTimeOfDay - 0.98f + nightLength) * (1 / 0.02f)));
+        else {
+            sun.intensity = 0;
         }
-
-        sun.intensity = sunInitialIntensity * intensityMultiplier;
     }
 }
